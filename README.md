22c508e0b76e579dc928df14a230b2c31056195a
Thorsten Kramm <tk@system42.io>
2022-09-15 20:14:19 +0200

    First implementation

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# rport-win-watchdog
-A watchdog for Windows that supervises the rport client
+## 🐶 A watchdog for the rport service on Windows
+
+A watchdog that supervises the rport service and restarts it when needed.
+The watchdog script is intended to be run every five minutes as a scheduled task.
+
+If you have the rport [watchdog integration](https://oss.rport.io/advanced/watchdog-integration/) enabled,
+this scripts checks the internal state of the rport client by reading the `state.json` file.
+
+If the rport client has not actively confirmed its successful connection to the rport server,
+the timestamp inside the json file will become obsolete. The scripts will compare this timestamp
+with the current time and restart the rport service if the given threshold is exceeded.
+
+### Installation & Usage
+
+Install from a PowerShell with administrative rights.
+
+```powershell
+cd "C:\Program Files\rport"
+iwr https://github.com/cloudradar-monitoring/rport-win-watchdog/releases/latest/watchdog.ps1 `
+-OutFile watchgog.ps1
+```
+
+Test, if the state file can be read:
+
+```powershell
+PS> .\watchdog.ps1 -Threshold 90
+09/15/2022 19:44:15: RPort is running fine. Last activity 27.7940099239349 seconds ago (< 90).
+```
+
+Before registering the scheduled task, read about [what's your best threshold](https://oss.rport.io/advanced/watchdog-integration/#implement-your-watchdog).
+
+Register the script as scheduled task:
+
+```powershell
+PS>  .\watchdog.ps1 -Threshold 300 -Register
+Registering scheduled task ...
+Task "RPort-Watchdog" ["C:\Program Files\rport\watchdog.ps1"] scheduled every 5 minutes.
+```
+
+### More options
+
+Unregister aka stop the watchdog:
+
+```powershell
+.\watchdog.ps1 -Unregister
+```
+
+Check, if it's working:
+
+```powerhsell
+PS> .\watchdog.ps1 -State
+Task RPort-Watchdog is registered with state: Ready
+
+
+LastRunTime        : 9/15/2022 7:54:54 PM
+LastTaskResult     : 0
+NextRunTime        : 9/15/2022 7:59:59 PM
+NumberOfMissedRuns : 0
+TaskName           : RPort-Watchdog
+TaskPath           :
+PSComputerName     :
+```
+
+Get the full help message:
+
+```powershell
+Get-Help .\watchdog.ps1 -Full
+```
--- watchdog.ps1
+++ watchdog.ps1
@@ -1,49 +1,103 @@
+<#
+.SYNOPSIS
+A watchdog that supervises the rport service and restarts it when needed.
+The watchdog script is intended to be run every five minutes as a scheduled task.
+
+This script is licensed under the MIT open-source license.
+.PARAMETER Threshold
+Use seconds
+.PARAMETER Register
+Register the script as a scheduled task. Requires -Threshold
+.PARAMETER Unregister
+Remove the scheduled task
+.PARAMETER State
+Print the state of the scheduled task
+.OUTPUTS
+System.String. Information about the state of rport and wether it has been restarted or not
+.EXAMPLE
+PS> watchdog.ps1 -Threshold 90
+09/15/2022 17:10:13: RPort is running fine. Last activity 3.52854990959167 seconds ago (< 90).
+.LINK
+https://github.com/cloudradar-monitoring/rport-win-watchdog
+https://oss.rport.io/advanced/watchdog-integration/
+#>
+#Requires -RunAsAdministrator
 Param(
     [switch]$Register,
     [switch]$Unregister,
+    [switch]$State,
     [int]$Threshold = 0
 )
 $ErrorActionPreference = "Stop"
-if ((-not $Unregister) -and ($Threshold -eq 0)) {
-    Write-Error "Threshold missing"
+
+Set-Variable dir -Option Constant -Value 'C:\Program Files\rport'
+Set-Variable stateFile -Option Constant -Value ($dir + '\data\state.json')
+Set-Variable logFile -Option Constant -Value ($dir + '\watchdog.log')
+Set-Variable taskName -Option Constant -Value "RPort-Watchdog"
+Set-Variable IntervallMin -Option Constant -Value 5
+
+function Write-Message($msg) {
+    <#
+    .SYNOPSIS
+    Write messages to a logfile
+    #>
+    Write-Output "$(Get-Date): $($msg)" | Out-File $logFile -Append
 }
 
-$dir = 'C:\Program Files\rport\'
-$stateFile = $dir + 'data\state.json'
-$logFile = $dir + 'watchdog.log'
-$taskName = "RPort-Watchdog"
-$IntervallMin = 5
-
-function Write-Log($msg) {
-    if (($env:USERPROFILE).Endswith("systemprofile")) {
-        Write-Output "$(Get-Date): $($msg)" | Out-File $logFile
-    }
-    else {
-        Write-Output $($msg)
-    }
-}
-
-function checkRport() {
-    if (-not (Test-Path $stateFile -PathType Leaf)){
-        Write-Log "ERROR: Statefile $($stateFile) not found. Not cheching."
+function Test-RportState {
+    <#
+    .SYNOPSIS
+    Test if rport is connected by reading the state.json file. If the last activity is older than the threshold, restart the service.
+    .OUTPUTS
+    System.ValueType Boolean
+    #>
+    if (-not (Test-Path $stateFile -PathType Leaf)) {
+        Write-Message "ERROR: Statefile $($stateFile) not found. Not cheching."
         return
     }
     $now = ((Get-Date -UFormat %s) - [int](Get-Date -UFormat %Z) * 3600)
     $lastUpdate = (Get-Content $stateFile | ConvertFrom-Json).last_update_ts
     $diff = $now - $lastUpdate
     if ($diff -gt $Threshold) {
-        Write-Log "RPort hangs. No activity deteced for $($diff) seconds (> $($Threshold)). Will restart service rport ..."
-        Restart-Service rport
+        Write-Message "RPort hangs. No activity deteced for $($diff) seconds (> $($Threshold)). Will restart service rport ..."
+        return $false
     }
     else {
-        Write-Log "RPort is running fine. Last activity $($diff) seconds ago (< $($Threshold))."
+        Write-Message "RPort is running fine. Last activity $($diff) seconds ago (< $($Threshold))."
+        return $true
     }
 }
 
-function registerTask() {
-    if ($null -ne (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue)){
+function Restart-Rport {
+    [CmdletBinding(SupportsShouldProcess)]
+    param()
+    <#
+    .SYNOPSIS
+    Restart rport but only if the service could not be started during boot or if it claims to be running.
+    This function does not restart rport if the service has been shut down properly.
+    #>
+    if ((Get-Service rport).Status -eq "Running") {
+        Restart-Service rport
+        Write-Message "RPort Service was running. RPort service restarted"
+        return
+    }
+    if (Test-LastStartupError) {
+        Restart-Service rport
+        Write-Message "RPort service has failed to start during last boot. RPort service restarted"
+        return
+    }
+    Write-Message "Not restarting RPort because it was intentionally stopped."
+}
+
+function Register-Task {
+    <#
+    .SYNOPSIS
+    Register this script as a sheduled task so it continuoly runs in the background
+    #>
+    Write-Output "Registering scheduled task ..."
+    if (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue) {
         Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
-        Write-Output "Existing scheduled task $($taskName) unregistered."
+        Write-Output "Existing scheduled task $($taskName) unregistered first."
     }
     $taskFile = '"C:\Program Files\rport\watchdog.ps1"'
     $description = 'A watchdog task that supervises the rport client by evaluating its state.json file'
@@ -60,18 +114,94 @@ function registerTask() {
         -Trigger $trigger `
         -Settings $settings `
         -Description $description
-    Register-ScheduledTask $taskName -InputObject $task|Out-Null
+    Register-ScheduledTask $taskName -InputObject $task | Out-Null
     Write-Output "Task `"$( $taskName )`" [$( $taskFile )] scheduled every $($IntervallMin) minutes."
 }
 
-if ($Register) {
-    registerTask
-}
-elseif ($Unregister) {
-    Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
-    Write-Output "Task unregistered"
-}
-else {
-    checkRport
+function Get-TaskState {
+    <#
+    .SYNOPSIS
+    Print the state of the scheduled task
+    #>
+    $state = (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue)
+    if ($state) {
+        Write-Output "Task $($taskName) is registered with state: $($state.state)"
+        $taskInfo = (Get-ScheduledTaskInfo -TaskName $taskName)
+        if($taskInfo.LastTaskResult -eq 267011) {
+            Write-Output "Task has not yet run."
+        }
+        elif($taskInfo.LastTaskResult -ne 0){
+            Write-Output "**CAUTION! The last execution of the task has failed.**"
+        }
+        $taskInfo
+    }
+    else {
+        Write-Output "Task $($taskName) is not registered as a sheduled task."
+    }
 }
 
+function Get-LastBoot {
+    <#
+    .SYNOPSIS
+    Return the time of the last system boot
+    .OUTPUTS
+    System.Object time of the last boot
+    #>
+    #Get-WmiObject win32_operatingsystem | Select-Object @{LABEL = 'Time'; EXPRESSION = { $_.ConverttoDateTime($_.lastbootuptime) } }
+    Get-CimInstance -ClassName win32_operatingsystem | Select-Object lastbootuptime
+}
+function Test-LastStartupError {
+    <#
+    .SYNOPSIS
+    Return true if the last startup of rport during the boot process failed due to a timeout
+    .OUTPUTS
+    System.ValueType Boolean
+    #>
+    $rportStartupErrors = Get-WinEvent -FilterHashtable @{
+        logname   = 'System'
+        StartTime = (Get-LastBoot).lastbootuptime
+        Level     = "2"
+    } | Where-Object { $_.Message -like '*rport*' }
+    $rportStartupErrors.Message | Select-String -Pattern "A time" -Quiet
+}
+
+if (-not(Get-Service rport -ErrorAction SilentlyContinue)) {
+    Write-Error "Service not found! This script requires rport to be registerd as a windows service."
+    exit 1
+}
+
+if ($Unregister) {
+    Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
+    Write-Output "Task unregistered"
+    exit 0
+}
+if ($State) {
+    Get-TaskState
+    exit 0
+}
+
+if ($Threshold -eq 0) {
+    Write-Error "Threshold missing. Use -Threshold N (seconds)"
+    exit 1
+}
+if ($Register) {
+    if ($PWD.Path -ne $dir) {
+        Write-Output "Copy script to $($dir) first and execute again from there."
+        exit 1
+    }
+    if (-not (Test-Path $stateFile -PathType Leaf)) {
+        Write-Error "State File Not Found! The file $($stateFile) does not exist. Enable the watchdog integration first."
+    }
+    Register-Task
+    exit 0
+}
+
+# Wipe the logfile before each run
+Clear-Content $logFile -ErrorAction SilentlyContinue
+if (-not (Test-RportState)) {
+    Restart-Rport
+}
+if (-not (($env:USERPROFILE).Endswith("systemprofile"))) {
+    # Print the logfile to the console
+    Get-Content $logFile
+}
